7f88a34e1774c59eff04321cde060de4d2b83bfa
Kevin Nolan <kevnolan@gmail.com>
2017-06-27 11:56:52 +0100

    Added line numbers

--- spacemacs.d/layers/a4b-lisp/packages.el
+++ spacemacs.d/layers/a4b-lisp/packages.el
@@ -24,15 +24,21 @@
       ;; Lispyville mode
       (add-hook 'emacs-lisp-mode-hook 'lispy-mode)
       (add-hook 'clojure-mode-hook 'lispy-mode)
+      (add-hook 'cider-mode-hook 'lispy-mode)
       (add-hook 'common-lisp-mode-hook 'lispy-mode)
       (add-hook 'scheme-mode-hook 'lispy-mode)
       (add-hook 'lisp-mode-hook 'lispy-mode)
       ;; Turn off smartparens
       (add-hook 'emacs-mode-hook #'turn-off-smartparens-mode)
       (add-hook 'clojure-mode-hook #'turn-off-smartparens-mode)
+      (add-hook 'cider-mode-hook #'turn-off-smartparens-mode)
       (add-hook 'common-lisp-mode-hook #'turn-off-smartparens-mode)
       (add-hook 'scheme-mode-hook #'turn-off-smartparens-mode)
-      (add-hook 'lisp-mode-hook #'turn-off-smartparens-mode))))
+      (add-hook 'lisp-mode-hook #'turn-off-smartparens-mode))
+    :config
+    (progn
+      ;; TODO make all letters more vim like
+      (lispy-define-key lispy-mode-map "m" #'lispy-view))))
 
 (defun a4b-lisp/init-lispyville ()
   (use-package lispyville
@@ -45,10 +51,11 @@
     :config
     (progn
       (message "lispyville config")
+      (lispy-define-key lispy-mode-map "v" #'lispyville-toggle-mark-type)
       (lispyville-set-key-theme '(operators
                                   escape
                                   s-operators
                                   additional-movement
                                   slurp/barf-cp
                                   additional
-                                  mark)))))
+                                  mark-toggle)))))
