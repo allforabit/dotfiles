de201217f67b9cdcf04e7e4e724a726ec5a4f003
Kevin Nolan <kevnolan@gmail.com>
2017-07-02 23:43:31 +0100

    Added snipe

--- spacemacs.d/layers/a4b-lisp/packages.el
+++ spacemacs.d/layers/a4b-lisp/packages.el
@@ -24,14 +24,14 @@
       ;; Lispyville mode
       (add-hook 'emacs-lisp-mode-hook 'lispy-mode)
       (add-hook 'clojure-mode-hook 'lispy-mode)
-      (add-hook 'cider-mode-hook 'lispy-mode)
+      (add-hook 'cider-repl-mode-hook 'lispy-mode)
       (add-hook 'common-lisp-mode-hook 'lispy-mode)
       (add-hook 'scheme-mode-hook 'lispy-mode)
       (add-hook 'lisp-mode-hook 'lispy-mode)
       ;; Turn off smartparens
       (add-hook 'emacs-mode-hook #'turn-off-smartparens-mode)
       (add-hook 'clojure-mode-hook #'turn-off-smartparens-mode)
-      (add-hook 'cider-mode-hook #'turn-off-smartparens-mode)
+      (add-hook 'cider-repl-mode-hook #'turn-off-smartparens-mode)
       (add-hook 'common-lisp-mode-hook #'turn-off-smartparens-mode)
       (add-hook 'scheme-mode-hook #'turn-off-smartparens-mode)
       (add-hook 'lisp-mode-hook #'turn-off-smartparens-mode))
@@ -54,7 +54,7 @@
       (lispy-define-key lispy-mode-map "v" #'lispyville-toggle-mark-type)
       (lispyville-set-key-theme '(operators
                                   escape
-                                  s-operators
+                                  ;; s-operators
                                   additional-movement
                                   slurp/barf-cp
                                   additional
