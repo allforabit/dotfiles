0d69c5ef6f260ac261d9474b2d46bd5ed75b639f
Kevin Nolan <kevnolan@gmail.com>
2018-03-29 15:30:27 +0100

    c:/Users/admin/Documents/GitHub/dotfiles/doom.d/custom-modes/a4b-web-browser/echo.js

--- doom.d/custom-modes/a4b-web-browser/echo.js
+++ doom.d/custom-modes/a4b-web-browser/echo.js
@@ -1,8 +1,17 @@
-var epc = require("elrpc");
+const epc = require("elrpc");
+const puppetteer = require("puppeteer");
 
 epc.startServer().then(function(server) {
-	server.defineMethod("echo", function(args) {
-		return args;
-	});
-	server.wait();
+
+  server.defineMethod("ss", function(path) {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    await page.screenshot({path: path});
+
+    await browser.close();
+    return args;
+  });
+  server.wait();
 });
