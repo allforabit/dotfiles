e5da317521a3fdff983074ce0435e8c288d8128b
Kevin Nolan <kevnolan@gmail.com>
2018-03-29 12:32:42 +0100

    Revert "c:/Users/admin/Documents/GitHub/dotfiles/doom.d/windows.el"
    This reverts commit ca346c723fe59d5e86a3ad780bc37189b672262e.

--- doom.d/windows.el
+++
@@ -1,7 +0,0 @@
-;;; windows.el --- Windows customisations -*- lexical-binding: t; -*-
-
-(setq shell-file-name (executable-find "zsh.exe"))
-(setenv "PATH" "C:\\programs\\msys2\\mingw64\\bin;C:\\programs\\msys2\\usr\\local\\bin;C:\\programs\\msys2\\usr\\bin;C:\\Windows\\System32;C:\\Windows")
-
-(provide 'windows)
-;;; windows.el ends here
