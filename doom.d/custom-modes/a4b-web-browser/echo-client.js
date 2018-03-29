5d51bfe921e83387a0498fe8c038caf9db6efa37
Kevin Nolan <kevnolan@gmail.com>
2018-03-29 15:17:46 +0100

    c:/Users/admin/Documents/GitHub/dotfiles/doom.d/custom-modes/a4b-web-browser/echo-client.js

--- doom.d/custom-modes/a4b-web-browser/echo-client.js
+++ doom.d/custom-modes/a4b-web-browser/echo-client.js
@@ -1,15 +1,5 @@
 var elrpc = require("elrpc");
 
-elrpc.startProcess(["node", "echo.js"]).then(function(client) {
-	client.callMethod("echo", "3 hello OK").then(function(ret) {
-		console.log(ret);
-		client.stop();
-	});
-	console.log("2 call hello");
-});
-
-
-console.log("1 start");
 (async function boot() {
   const client = await elrpc.startProcess(["node", "echo.js"]);
   const result = await client.callMethod("echo", "3 hello OK");
