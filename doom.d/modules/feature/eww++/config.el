c705f234441983abaf7d560b9a7da2152b2dfb03
Kevin Nolan <kevnolan@gmail.com>
2018-04-02 16:05:37 +0100

    doom.d/modules/feature/eww++/config.el

---
+++ doom.d/modules/feature/eww++/config.el
@@ -0,0 +1,5 @@
+;;; config.el -*- lexical-binding: t; -*-
+
+;; Fix BG
+;; https://emacs.stackexchange.com/questions/2955/how-to-customize-background-color-for-some-web-pages-opened-with-eww
+(setq shr-color-visible-luminance-min 70)
