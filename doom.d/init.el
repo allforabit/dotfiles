25df01aa2f7d3384a8cee4ea017c665f3d452943
Kevin Nolan <kevnolan@gmail.com>
2018-05-15 19:26:34 +0100

    Remove modeline from windows

--- doom.d/init.el
+++ doom.d/init.el
@@ -32,7 +32,7 @@
        :ui
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-modeline     ; a snazzy Atom-inspired mode-line
+       (unless IS-WINDOWS doom-modeline)     ; a snazzy Atom-inspired mode-line
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        evil-goggles      ; display visual hints when editing in evil
        hl-todo           ; highlight TODO/FIXME/NOTE tags
